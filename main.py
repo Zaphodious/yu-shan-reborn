7ba065961b965d8b6314ec12a28cc3af2e0082ce
Alex Chythlook <zaphodious@users-MacBook-Pro.local>
2019-02-13 12:58:43 -0900

    Play around with type decs

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import fastapi
+from typing import List, Set, Dict, Tuple, Optional
+import pydantic
 
 app = fastapi.FastAPI()
+
+class Entity(pydantic.BaseModel) :
+    thing:int
+    another:str = None
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/")
+def put_item(entity: Entity):
+    print(entity)
